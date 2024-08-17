2ec444e9aabd90f901868249d77d95c816d372a1
Pinkdragon <cherandwhite@gmail.com>
2024-08-17 16:30:22 -0400

    Updates

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 const quotesByMood = {
-    happy: [
-        "Happiness is not by chance, but by choice. - Jim Rohn",
-        "Happiness is the best makeup. - Drew Barrymore",
-        "The purpose of our lives is to be happy. - Dalai Lama"
+    joy: [
+        "Do you ever look at someone and wonder, what is going on inside their head?",
+        "Think positive!",
+        "All right! We did not die today, I call that an unqualified success!",
+        "Find the fun!",
+        "You can't focus on what's going wrong. There's always a way to turn things around."
+        
     ],
     sad: [
-        "Tears come from the heart and not from the brain. - Leonardo da Vinci",
-        "Every human walks around with a certain kind of sadness. They may not wear it on their sleeves, but it's there if you look deep. - Taraji P. Henson",
-        "Sadness flies away on the wings of time. - Jean de La Fontaine"
+       "Crying helps me slow down and obsess over the weight of life's problems.",
+       "I'm too sad to walk. Just give me a few... hours.",
+       "I’m sorry. I’m just sad.",
+       "We could cry until we can’t breathe.",
+       "I'm not going to make it in time."
+
     ],
     motivated: [
-        "The only way to do great work is to love what you do. - Steve Jobs",
-        "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
-        "Don’t watch the clock; do what it does. Keep going. - Sam Levenson"
+        "Congratulations, San Francisco, you’ve ruined pizza! First the Hawaiians, and now you!",
+        "Oh, I'll show you attitude, old man!",
+        "Don't touch me!",
+        "You wanna piece of this, pops?",
+        "Can I say that curse word now?"
+
+    ],
+    digust: [
+        "I'm telling you, it smells like something died in here.",
+        "That is not brightly colored or shaped like a dinosaur. Hold on, guys. It’s broccoli!",
+        "I just saved our lives. Yeah, you’re welcome.",
+        "Seriously?",
+        "Does anyone smell that?"
+
+    ],
+    fear: [
+        "It’s broccoli! Ewww!",
+        "Oh, I show you attitude, old man!",
+        "We did not die today. I call that an unqualified success!",
+        "That’s it. I’m done.",
+        "What was that?!"
+
+
     ],
     
 };
@@ -25,7 +51,7 @@ document.getElementById('generateQuoteBtn').addEventListener('click', function()
         const randomIndex = Math.floor(Math.random() * quotes.length);
         document.getElementById('quoteText').textContent = quotes[randomIndex];
     } else {
-        document.getElementById('quoteText').textContent = "Sorry, no quotes found for that mood. Try 'happy', 'sad', or 'motivated'.";
+        document.getElementById('quoteText').textContent = "Sorry, no quotes found for that mood. Try 'joy', 'sad', or 'fear'.";
     }
 });
 const submittBtn= document.getElementById('submittBtn');
